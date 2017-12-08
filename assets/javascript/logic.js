d5d5a68b2ab7598fc233b88fc9c5a7db4f54ff10
mathewhall100 <mathew.hall@dr.com>
2017-12-08 17:39:26 -0500

    ajax requests added and js file structured

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,326 @@
-/*Javascript for Ajax Giphy Homework game (Week 6 Homework )*/
+
+/*Javascript for Travelsafe application (Project 1)*/
+
+
+$(document).ready(function() {
+
+
+/*  --------------------------- Global variables -------------------------------------*/
+
+
+var countryInput = "";
+var countryResults = [];
+
+
+/*  --------------------------- Objects ----------------------------------------------*/
+
+
+
+
+
+/*  --------------------------- functions ---------------------------------------------*/
+
+
+// get road traffic deaths rates
+
+function get_rta_data (code) {
+
+    var rtaURL = "http://apps.who.int/gho/athena/data/GHO/RS_196,RS_198.json?profile=simple&filter=COUNTRY:*";
+    var queryURL = rtaURL.replace("COUNTRY:*", "COUNTRY:" + code);  // replace 'country:*'' with 'country:code'
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET',
+      crossDomain: true,
+      dataType: 'jsonp',
+
+    })
+
+    .done(function(response) {
+
+        if (response.fact[0].dim.GHO.indexOf("per 100 000 population") > 0) { rtaData = response.fact[0].Value; }
+           else { rtaData = response.fact[1].Value; }
+
+        console.log(code + ": Annual road traffic deaths (/100,000 population 2016) = " + rtaData);
+
+        // check to see if result returned
+        // display raw result with explanation to div in UX
+        // calculate ranking 1-10 
+        // place ranking result to countryResults[] array
+
+    });
+
+}
+
+
+ // get air pollution data
+
+function get_airpoll_data (code) { 
+
+    var airpollURL = "http://apps.who.int/gho/athena/data/GHO/AIR_5,AIR_50,AIR_41.json?profile=simple&filter=COUNTRY:*;REGION:*";
+    var queryURL = airpollURL.replace("COUNTRY:*", "COUNTRY:" + code);  // replace 'country:*'' with 'country:code'
+
+    $.ajax({
+    url: queryURL,
+    method: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+
+    })
+
+    .done(function(response) {
+
+        var airpollData = response.fact[0].Value; 
+
+        console.log(code + ": Air pollution annual deaths (number)= " + airpollData);
+
+        // check to see if result returned
+        // display raw result with explanation to div in UX
+        // calculate ranking 1-10 
+        // place ranking result to countryResults[] array
+
+    });
+
+}
+
+
+// get natural disaster data 
+
+function get_natdis_data (code) {
+
+    var natdisURL = "http://apps.who.int/gho/athena/data/GHO/SDGDISASTER.json?profile=simple&filter=COUNTRY:*;REGION:*";
+    var queryURL = natdisURL.replace("COUNTRY:*", "COUNTRY:" + code);  // replace 'country:*'' with 'country:code'
+
+    $.ajax({
+    url: queryURL,
+    method: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+
+    })
+
+    .done(function(response) {
+
+        var natdisData = response.fact[0].Value; 
+
+        console.log(code + ": Average deaths from natural disasters (/100,000 population (2011-2015) = " + natdisData);
+
+        // check to see if result returned
+        // display raw result with explanation to div in UX
+        // calculate ranking 1-10 
+        // place ranking result to countryResults[] array
+
+    });
+
+}
+
+
+// get water hygeine data 
+
+function get_hygeine_data (code) {
+
+    var hygeineURL = "http://apps.who.int/gho/athena/data/GHO/SDGWSHBOD.json?profile=simple&filter=COUNTRY:*";
+    var queryURL = hygeineURL.replace("COUNTRY:*", "COUNTRY:" + code);  // replace 'country:*'' with 'country:code'
+
+    $.ajax({
+    url: queryURL,
+    method: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+
+    })
+
+    .done(function(response) {
+
+        var hygeineData = response.fact[0].Value; 
+
+        console.log(code + ": Deaths attributed to unsafe water hygeine (/100,000 population (year) = " + hygeineData);
+
+        // check to see if result returned
+        // display raw result with explanation to div in UX
+        // calculate ranking 1-10 
+        // place ranking result to countryResults[] array
+
+    });
+
+
+}
+    
+// get homocide rates
+
+function get_homocide_data (code) {
+
+    var homocideURL = "http://apps.who.int/gho/athena/data/GHO/VIOLENCE_HOMICIDENUM,VIOLENCE_HOMICIDERATE.json?profile=simple&filter=COUNTRY:*;AGEGROUP:-;SEX:-";
+    var queryURL = homocideURL.replace("COUNTRY:*", "COUNTRY:" + code);  // replace 'country:*'' with 'country:code
+
+    $.ajax({
+    url: queryURL,
+    method: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+
+    })
+
+    .done(function(response) {
+
+        var homocideData = response.fact[0].Value; 
+
+        console.log(code + ": Homocides (/100,000 population (year) = " + homocideData);
+
+        // check to see if result returned
+        // display raw result with explanation to div in UX
+        // calculate ranking 1-10 
+        // place ranking result to countryResults[] array
+
+    });
+
+}
+
+// get comm disesases data 
+
+function get_commdiseases_data (code) {
+
+    var commdiseasesURL = "http://apps.who.int/gho/athena/data/GHO/SDGHIV,SDGMALARIA,SDGNTDTREATMENT,MDG_0000000020,WHS4_117.json?profile=simple&filter=COUNTRY:*;YEAR:2016;YEAR:2015;YEAR:2014;YEAR:2013;YEAR:2012;YEAR:2011;YEAR:2010;YEAR:2005;YEAR:2000";
+    var queryURL = commdiseasesURL.replace("COUNTRY:*", "COUNTRY:" + code);  // replace 'country:*'' with 'country:code'
+
+    $.ajax({
+    url: queryURL,
+    method: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+
+    })
+
+    .done(function(response) {
+
+        console.log(response);
+
+        //var malariaData = response.fact[0].Value;
+        //var hivData = response.fact[0].Value;
+        //var ntdData = response.fact[0].Value;
+
+        //console.log(code + ": Malaria (/100,000 population (year) = " + hygeineData);
+        //console.log(code + ": Malaria (/100,000 population (year) = " + hygeineData);
+        //console.log(code + ": Malaria (/100,000 population (year) = " + hygeineData);
+  
+        // check to see if result returned
+        // display raw result with explanation to div in UX
+        // calculate ranking 1-10 
+        // place ranking result to countryResults[] array
+
+    });
+
+}
+
+    // get health personnel data
+
+    function get_commdiseases_data (code) {
+
+    var healthworkersURL = "http://apps.who.int/gho/athena/data/GHO/HRH_26,HRH_33,HRH_28,HRH_25,HRH_27,HRH_31,HRH_29,HRH_30,HRH_32.json?profile=simple&filter=COUNTRY:*";
+    var queryURL = healthworkersURL.replace("COUNTRY:*", "COUNTRY:" + code);  // replace 'country:*'' with 'country:code'
+
+    $.ajax({
+    url: queryURL,
+    method: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+
+    })
+
+    .done(function(response) {
+
+        console.log(response);
+
+        // var physiciansData = response.fact[0].Value;
+        // var nursesData = response.fact[0].Value;
+        // var dentistsData = response.fact[0].Value;
+
+        //console.log(code + ": Physicians  (/100,000 population (year) = " + physiciansData);
+        //console.log(code + ": Nurses (/100,000 population (year) = " + nursesData);
+        //console.log(code + ": Dentists (/100,000 population (year) = " + dentistsData);
+
+        // check to see if result returned
+        // display raw result with explanation to div in UX
+        // calculate ranking 1-10 
+        // place ranking result to countryResults[] array
+
+    });
+
+
+    // Ajax request error handling here
+
+}
+
+
+function set_up () {
+
+    // set up page as required
+}
+
+
+function display_searches () {
+
+    // load from firebase and display results of previous searches on page
+}
+
+
+function calculate_rank () {
+
+  // use the data to calculate the ranking of user input country
+} 
+
+
+function display_rank () {
+
+   // call function to display ranking
+
+}
+
+
+function interpret_rank () {
+
+   // call function to interpet rankings 
+
+}
+
+
+function top_ranks () {
+
+    // check firebase db for recent record of top/bottom 5 ranked countries- if found load and use
+    // otherwise calculate the top 5 ranked country rankings (dispaly initialising " message ..."")
+    // and the bottom 5 ranked countries
+    // and display on page
+    // calculate average ranking for top 5 and bottom 5 countries 
+    //     and place them in global variables to use to calculate other country rankings on a scale top-bottom
+    // store in firebase with timestamp to recalculate every week or month or so. 
+}
+
+
+function save_searche () {
+
+    // save searches to firebase
+}
+
+
+/*  --------------------------- Calls ---------------------------------------------*/
+
+set_up (); // set up page
+
+top_ranks (); // get top and bottom ranking countries and dispaly
+
+display_searches ();  // if user logged in load and dispaly users previous searches from firebase
+                      // dispaly message asking user to register/login in if not already done so
+
+
+// then listen for user to input a country to search
+
+
+
+/*  --------------------------- Event listeners ---------------------------------------------*/
+
 
 // Autocomplete Country input AND code generation based on input
+
 $(function() {
     $('#country').typeahead({
         source: countries
@@ -10,201 +330,45 @@ $(function() {
         console.log(country);
         $('#result').show();
         $('#country-name').text(country.name);
-        $('#country-code').text(country.alpha3Code)
-    });
-});
+        $('#country-code').text(country.alpha3Code);
 
+        countryInput = country.alpha3Code;
 
+       // call all the ajax requests
 
+        var countryRTAData = get_rta_data (countryInput);
+        var countryAirpollData = get_airpoll_data(countryInput);
+        var counryNatdisData = get_natdis_data(countryInput);
+        var hygeineData = get_hygeine_data(countryInput);
+        var homocideData = get_homocide_data(countryInput);
+        var commdiseasesdata = get_commdiseases_data(countryInput);
+        var healthworkersData = get_healthworkers_data(countryInput);
 
-$(document).ready(function() {
+        //set interval timer to wait for countryResults[] arraty to be full then move on to rankig calculations using data in countryResults[] 
 
+        var intervalId = setInterval( function() { 
 
-/*  --------------------------- Global variables ---------------------------------------------*/
-/*  --------------------------- Global variables ---------------------------------------------*/
-/*  --------------------------- Global variables ---------------------------------------------*/
+                while (countryResults.length < x /*total size data set*/ ) { /* dispaly "waiting .... " message in UX */ 
+                                                                             /*continue checking every 100ms*/
+                                                                             /*check to make sure a max time not exceeded as well*/                          
+                }
 
-var countryCodes = {afghanistan:"AFG", america:"USA", usa:"USA"}
+          }, 100);
 
+        intervalId.clear();
 
-    //cummicable diseases (hepB, TB, malaria, NTDs, HIV)
-    //var commdisURL = "http://apps.who.int/gho/athena/data/GHO/SDGHIV,SDGMALARIA,SDGNTDTREATMENT,MDG_0000000020,WHS4_117.json?profile=simple&filter=COUNTRY:*;YEAR:2016;YEAR:2015;YEAR:2014;YEAR:2013;YEAR:2012;YEAR:2011;YEAR:2010;YEAR:2005;YEAR:2000"
+        /* call calculate-rank() */
+        /* call display-rank() */
+        /* call interpret-rank() */
+        /* if user logged in call save-search() */
 
-    //new HIV cases/yr /1000popultaion
-    //var hivURL = "http://apps.who.int/gho/athena/data/GHO/HIV_0000000026,SDGHIV.json?profile=simple&filter=COUNTRY:*;REGION:*"
-
-    //malaria cases/year
-    //var malariaURL = "http://apps.who.int/gho/athena/data/GHO/MALARIA002.json?profile=simple&filter=COUNTRY:*"
-
-    // reported cases of cholera
-    //var choleraURL = "http://apps.who.int/gho/athena/data/GHO/CHOLERA_0000000001.json?profile=simple&filter=COUNTRY:*;REGION:*"
-
-    //deaths from ambient air pollution
-    //var airpollURL = "http://apps.who.int/gho/athena/data/GHO/AIR_5,AIR_50,AIR_41.json?profile=simple&filter=COUNTRY:*;REGION:*"
-
-    //death rate from natural disasters (/100,000population)
-    //var natdisURL = "http://apps.who.int/gho/athena/data/GHO/SDGDISASTER.json?profile=simple&filter=COUNTRY:*;REGION:*";
-
-    //mortality rate attributable to unsafe water, sanination and hygeine (/100,000)
-    //var waterURL = "http://apps.who.int/gho/athena/data/GHO/SDGWSHBOD.json?profile=simple&filter=COUNTRY:*";
-
-    //road traffic deaths/100,000 population
-    //var rtaURL = "http://apps.who.int/gho/athena/data/GHO/RS_196,RS_198.json?profile=simple&filter=COUNTRY:*";
-
-    //homocide rates
-    //var homocideURL = "http://apps.who.int/gho/athena/data/GHO/VIOLENCE_HOMICIDENUM,VIOLENCE_HOMICIDERATE.json?profile=simple&filter=COUNTRY:*;AGEGROUP:-;SEX:-"
-
-    // healthcare personal per 100,000 population
-    //var healthcareURL = "http://apps.who.int/gho/athena/data/GHO/HRH_26,HRH_33,HRH_28,HRH_25,HRH_27,HRH_31,HRH_29,HRH_30,HRH_32.json?profile=simple&filter=COUNTRY:*"
-
-
-/*    //US GOv acute travel alerts (XML)
-    var newalertsURL="https://travel.state.gov/_res/rss/TAs.xml";
-
-    //US gov chronic travel alerts (xml)
-    var allalertsURL="https://travel.state.gov/_res/rss/TWs.xml";*/
-
-
-// Event listeners
-
-
-$('#country-submit').on('click', function () {
-    var countryName = ('#country-input').val();
-
-    var country = country_lookup(countryName);
-
-
-     // get air pollution data 
-    var commdisURL = "http://apps.who.int/gho/athena/data/GHO/SDGHIV,SDGMALARIA,SDGNTDTREATMENT,MDG_0000000020,WHS4_117.json?profile=simple&filter=COUNTRY:*;YEAR:2016;YEAR:2015;YEAR:2014;YEAR:2013;YEAR:2012;YEAR:2011;YEAR:2010;YEAR:2005;YEAR:2000";
-    var queryURL = commdisURL + country;  // may need furtehr processing
-    var data = ajax_call (queryURL);
-
-    var malariaDat = data;
-    var hivDat = data;
-    var ntdDat = data;
-
-
-     // get air pollution data 
-    var airpollURL = "http://apps.who.int/gho/athena/data/GHO/AIR_5,AIR_50,AIR_41.json?profile=simple&filter=COUNTRY:*;REGION:*"
-    var queryURL = airpollURL + country;  // may need furtehr processing
-    var data = ajax_call (queryURL);
-
-    var airpollDat = data;
-
-
-    // get natural disaster data 
-    var natdisURL = "http://apps.who.int/gho/athena/data/GHO/SDGDISASTER.json?profile=simple&filter=COUNTRY:*;REGION:*";
-    var queryURL = natdisURL + country;  // may need furtehr processing
-    var data = ajax_call (queryURL);
-
-    var natdisDat = data;
-
-
-    // get water hygeine data 
-    var waterURL = "http://apps.who.int/gho/athena/data/GHO/SDGWSHBOD.json?profile=simple&filter=COUNTRY:*";
-    var queryURL = waterURL + country;  // may need furtehr processing
-    var data = ajax_call (queryURL);
-
-    var waterDat = data;
-
-
-    // get road traffic deaths rates
-    var rtaURL = "http://apps.who.int/gho/athena/data/GHO/RS_196,RS_198.json?profile=simple&filter=COUNTRY:*";
-    var queryURL = rtaURL + country;  // may need furtehr processing
-    var data = ajax_call (queryURL);
-
-    var homocideDat = data;
-
-    
-    // get homocide rates
-    var homocideURL = "http://apps.who.int/gho/athena/data/GHO/VIOLENCE_HOMICIDENUM,VIOLENCE_HOMICIDERATE.json?profile=simple&filter=COUNTRY:*;AGEGROUP:-;SEX:-"
-    var queryURL = homocideURL + country;  // may need furtehr processing
-    var data = ajax_call (queryURL);
-
-    var homocideDat = data;
-
-
-    // get health personnel data
-    var healthcareURL = "http://apps.who.int/gho/athena/data/GHO/HRH_26,HRH_33,HRH_28,HRH_25,HRH_27,HRH_31,HRH_29,HRH_30,HRH_32.json?profile=simple&filter=COUNTRY:*"
-    var queryURL = healthcareURL + country;
-    var data = ajax_call (queryURL);
-
-    var physiciansDat = data;
-    var dentistsDat = data;
-    var nursesDat = data;
-
-
-    calculateRank (); // use the data to calculate the ranking
-
-    displayData (); // call function to dispaly data
-
-
-
-});
-
-
-// functions
-
-function ajax_call (msg) {
-
-    $.ajax({
-      url: msg,
-      method: 'GET',
-      crossDomain: true,
-      dataType: 'jsonp',
-
-    })
-
-    .done(function(response) {
-      console.log(response);
-      return response;
     });
 
-}
-
-function country_lookup (name) {
-
-    // function to retrun country cose from name
-    // if no country code found - return invalid country
-
-}
-
-function top_ranks () {
-
-    // function to calculate the top 5 ranked country rankings
-    // and the bottom 5 ranked countries
-    // and display on page
-    // set global variable top and bottom rankings for reference
-}
-
-function load_searches () {
-
-    // load past searchges from firebase
-}
-
-function save_searches () {
-
-    // save searches to firebase
-}
-
-function display_searches () {
-
-    // display results of previous searches on page
-}
-
-
-// calls
-
-
-
-top_ranks ();
-
-load_searches ();
-
-display_searches ();
-
-
-
-
-
 });
+
+
+
+
+
+
+});  // close of document ready function.
