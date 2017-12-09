14ff1387a716061cf08c528ccf321a52bb77126a
Jordan Isenberg <jordanisen@gmail.com>
2017-12-09 10:40:44 -0500

    adding firebase js file and call to FB in index file

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,14 @@
+// Javascript File to save input and API data to Firebase
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB1zD5vvuhDY0N2PLKgMkIqLCLcz8fqeIk",
+    authDomain: "travelsafe-4a936.firebaseapp.com",
+    databaseURL: "https://travelsafe-4a936.firebaseio.com",
+    projectId: "travelsafe-4a936",
+    storageBucket: "travelsafe-4a936.appspot.com",
+    messagingSenderId: "1075160018454"
+  };
+  
+  firebase.initializeApp(config);
